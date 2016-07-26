04271743aeba2bbbf86a82d907693e9bd3e13aeb
nruest <ruestn@gmail.com>
2016-07-26 09:07:25 -0400

    WHITE SPAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAACE

--- src/Uuid/UuidGenerator.php
+++ src/Uuid/UuidGenerator.php
@@ -6,9 +6,9 @@
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  *
- * PHP Version 5.5.9           
+ * PHP Version 5.5.9
  *
- * @category Islandora         
+ * @category Islandora
  * @package  Islandora
  * @author   Daniel Lamb <dlamb@islandora.ca>
  * @license  https://opensource.org/licenses/MIT MIT
